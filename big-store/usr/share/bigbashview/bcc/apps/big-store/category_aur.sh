b68b3214063d851d43dcccdd08b91797d573e988
Pedro Maimere <74515218+maimere@users.noreply.github.com>
2022-08-09 10:28:44 -0300

    Parcial 2

--- big-store/usr/share/bigbashview/bcc/apps/big-store/category_aur.sh
+++ big-store/usr/share/bigbashview/bcc/apps/big-store/category_aur.sh
@@ -26,22 +26,28 @@ PKG="$@"
 # LANGUAGE=C yay -a -Sii $(cat /tmp/bigstore/category_aur.txt | tr "\n" " ")
 
 LANGUAGE=C yay -a -Sii $@ | \
-gawk ''
-## Remover uma das aspas
+gawk -v instalar=$"Instalar" -v remover=$"Remover" -- ''
+########################################################################### Remover uma das aspas
 
-#BEGIN {
-#    FS = " +: "
-#}
+BEGIN {
+    OFS = "\n"
+}
 
+# Following block runs when blank line found, i.e., on the transition between packages
 !$0 {
-    title = version = description = installed = ""
+    title = version = description = installed = idaur = button = ""
 }
 
 /^Name/ {
     title = gensub(/^Name +: /,"",1)
     not_installed = system("pacman -Q " title)
     if ( not_installed ) {
-        idaur = "id=\"AurP2\">"
+        idaur = "AurP2"
+        button = "<div id=aur_not_installed>" instalar "</div></a></div></div>"
+    } else {
+        idaur = "AurP1"
+        button = "<div id=aur_installed>" remover "</div></a></div></div>"
+    }
 }
 
 /^Version/ {
@@ -52,13 +58,45 @@ gawk ''
     description = gensub(/^Description +: /,"",1)
 }
 
+# When all variables are set
+title && version && description && installed && idaur && button {
+    if ( system("[ ! -e icons/" title ".png ]") ) {
+        icon = "<img class=\"icon\" src=\"icons/" title ".png\">"
+    } else {
+        icon = "<div class=avatar_aur>" substr(title,1,3) "</div>"
+    }
+    
+# Checking custom localized description
+    shortlang = gensub(/\..+/,"",1,ENVIRON["LANG"])
+    summaryfile = "description/" title "/" shortlang "/summary"
+# Double negative because system() returns exit status of shell command inside ()
+    if ( !system("[ -e " summaryfile " ]") ) {
+        RS_BAK = RS
+        RS = "^$"
+        getline description < summaryfile
+        close(summaryfile)
+        RS = RS_BAK
+    }
 
+# Writes html of current package on aurbuild.html
+# Do not worry, file redirector ">" works different in awk: only the first interaction deletes file content
+    print(\
+"<a onclick=\"disableBody();\" href=\"view_aur.sh.htm?pkg_name=" title "\">",
+"<div class=\"col s12 m6 l3\" id=" idaur ">",
+"<div class=\"showapp\">",
+"<div id=aur_icon><div class=icon_middle>" icon "</div>",
+"<div id=aur_name><div id=limit_title_name>" title "</div>",
+"<div id=version>" version "</div></div></div>",
+"<div id=box_aur_desc><div id=aur_desc>" description "</div></div>",
+button) > tmpfolder "/aurbuild.html"
+
+}
 
 #for i  in  $(LANGUAGE=C yay -a -Sii $@ | grep -e ^Name -e ^Version -e ^Description | cut -f2-10 -d":" | awk '{if (NR%3) {ORS="";print "|"$0} else {ORS="\n";print "|"$0}}' | sed 's/^| //g;s/| /|/g'); do
 #    TITLE="$(echo "$i" | cut -f1 -d"|")"
 #    VERSION="$(echo "$i"| cut -f2 -d"|")"
 #    DESCRIPTION="$(echo "$i" | cut -f3 -d"|")"
-    INSTALLED="$(pacman -Q "$TITLE" 2> /dev/null)"
+#    INSTALLED="$(pacman -Q "$TITLE" 2> /dev/null)"
     echo "<a onclick=\"disableBody();\" href=\"view_aur.sh.htm?pkg_name=$TITLE\">" >> ${TMP_FOLDER}/aurbuild.html
     echo '<div class="col s12 m6 l3"' >> ${TMP_FOLDER}/aurbuild.html
     if [ "$INSTALLED" = "" ]; then
@@ -84,15 +122,15 @@ gawk ''
         echo "<div id=box_aur_desc><div id=aur_desc>$DESCRIPTION</div></div>" >> ${TMP_FOLDER}/aurbuild.html
     fi
 
-    if [ "$INSTALLED" = "" ]; then
-        echo '<div id=aur_not_installed>' $"Instalar" '</div></a></div></div>' >> ${TMP_FOLDER}/aurbuild.html
-    else
-        echo '<div id=aur_installed>' $"Remover" '</div></a></div></div>' >> ${TMP_FOLDER}/aurbuild.html
-    fi
+#    if [ "$INSTALLED" = "" ]; then
+#        echo '<div id=aur_not_installed>' $"Instalar" '</div></a></div></div>' >> ${TMP_FOLDER}/aurbuild.html
+#    else
+#        echo '<div id=aur_installed>' $"Remover" '</div></a></div></div>' >> ${TMP_FOLDER}/aurbuild.html
+#    fi
 let COUNT=COUNT+1; 
-done
+#done
 
-IFS=$OIFS
+#IFS=$OIFS
 
 
 if [ "$COUNT" -gt "0" ]; then
